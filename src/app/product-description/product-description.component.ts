84db1127d045bf4fd11848a6c8467744f19f30ad
WakkoJoe <wakkojoe@gmail.com>
2018-02-02 20:26:11 -0500

    import service
    changes to import service into description component

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
+  private albumInfo: produc
+  constructor(productService: ProductService) { }
 
   ngOnInit() {
+
   }
 
 }
